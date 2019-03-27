72627893c0b0239c9b3b06927e46a553bf0ed92e
markandevansh99 <500069794@stu.upes.ac.in>
2019-03-27 12:16:21 +0530

    3rd Commit

--- hello1.cpp
+++ hello1.cpp
@@ -45,4 +45,12 @@ void Input()
  cout<<"\nEnter Type::";
  gets(otype);
  initprice();
-}}
+void display()
+{cout<<"\n\nRESULT";
+ cout<<"\n\nCode="<<ocode;
+ cout<<"\n\nSize="<<osize;
+ cout<<"\n\nFabric="<<ofabric;
+ cout<<"\n\nType="<<otype;
+ cout<<"\n\nPrice="<<oprice;
+}
+};
