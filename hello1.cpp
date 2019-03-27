c7383a0a5290955d0aa57db3623b88264b2e4e96
markandevansh99 <500069794@stu.upes.ac.in>
2019-03-27 12:15:12 +0530

    Commit number2

--- hello1.cpp
+++ hello1.cpp
@@ -25,4 +25,24 @@ class Outfit
  (strcmp(otype,"Jacket")==0)
  oprice=2500-(2500/4);
  }
+public:
+Outfit()
+{
+ strcpy(ocode,"not initialised");
+ strcpy(otype,"not initialised");
+ strcpy(ofabric,"not initialised");
+ osize=0;
+ oprice=0;
 }
+void Input()
+{
+ cout<<"\nEnter Code::";
+ gets(ocode);
+ cout<<"\nEnter Size::";
+ cin>>osize;
+ cout<<"\nEnter Fabric::";
+ gets(ofabric);
+ cout<<"\nEnter Type::";
+ gets(otype);
+ initprice();
+}}
